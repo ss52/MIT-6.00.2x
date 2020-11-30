90fcc13b08efb2c60d99bb107a6628cc7aedc3c5
Aleksandr Markelov <smarkelov@gmail.com>
2020-11-30 12:29:46 +0300

    Lecture 8 tasks

--- Unit_3/lecture_8.py
+++ Unit_3/lecture_8.py
@@ -13,3 +13,23 @@ def throwNeedles(numNeedles):
             inCircle += 1
 
     return 4 * (inCircle / float(numNeedles))
+
+
+def noReplacementSimulation(numTrials):
+    res = 0
+
+    for trial in range(numTrials):
+        bag = [0, 0, 0, 1, 1, 1]
+        sum = 0
+        for i in range(3):
+            ball_number = random.randint(0, len(bag) - 1)
+            ball = bag.pop(ball_number)
+            sum += ball
+
+        if sum == 3 or sum == 0:
+            res += 1
+
+    return res / numTrials
+
+
+# print(noReplacementSimulation(5000))
